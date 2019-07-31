69297bfd370090b6c6728a17d3de7011627b1603
Vladimir Nickolaev <exlnted@gmail.com>
2019-07-31 14:11:48 +0300

    add build

---
+++ build/precache-manifest.1aa08b9c838ed2e245130253a810a521.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "450c94f2b09521926aef",
+    "url": "/static/js/main.fbebfa33.chunk.js"
+  },
+  {
+    "revision": "72a510a01b0726eb88d5",
+    "url": "/static/js/2.cbc328e9.chunk.js"
+  },
+  {
+    "revision": "450c94f2b09521926aef",
+    "url": "/static/css/main.fc878e26.chunk.css"
+  },
+  {
+    "revision": "3ec69861a3709c1801a91eca1b4bf526",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.b445d3f7ea95c2b9e6048866191e42fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "de103897e3bd58792809",
+    "url": "/static/js/main.fbebfa33.chunk.js"
+  },
+  {
+    "revision": "72a510a01b0726eb88d5",
+    "url": "/static/js/2.cbc328e9.chunk.js"
+  },
+  {
+    "revision": "de103897e3bd58792809",
+    "url": "/static/css/main.08e59cb6.chunk.css"
+  },
+  {
+    "revision": "b7d4f89239cfbfac12225f7b704f261d",
+    "url": "/index.html"
+  }
+];
